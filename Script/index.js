cf1ca0d2d4e56020a7adf72db95e24f18a2f9237
Administrator <thomasfilliol78@gmail.com>
2016-10-03 18:33:41 +0200

    test Commit

--- Script/index.js
+++ Script/index.js
@@ -27,6 +27,5 @@ function mailIsValid(mail){
 }
 
 function mdpIsValid(mdp){
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(mdp);
+    return true;
 }
